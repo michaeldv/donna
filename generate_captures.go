92982a36b9eb8aa22ca274c843ea126d2136a055
Michael Dvorkin <mike@dvorkin.net>
2014-02-06 21:46:42 -0800

    Simplification

--- generate_captures.go
+++ generate_captures.go
@@ -6,9 +6,7 @@ package donna
 
 func (gen *MoveGen) GenerateCaptures() *MoveGen {
         color := gen.p.color
-        gen.pawnCaptures(color)
-        gen.pieceCaptures(color)
-        return gen
+        return gen.pawnCaptures(color).pieceCaptures(color)
 }
 
 // Generates all pseudo-legal pawn captures and Queen promotions.
@@ -16,10 +14,10 @@ func (gen *MoveGen) pawnCaptures(color int) *MoveGen {
         pawns := gen.p.outposts[Pawn(color)]
 
         for pawns != 0 {
-                square := pawns.pop()
                 //
                 // First check capture targets on rows 2-7 (no promotions).
                 //
+                square := pawns.pop()
                 targets := gen.p.targets[square] & gen.p.board[color^1] & 0x00FFFFFFFFFFFF00
                 for targets != 0 {
                         gen.list[gen.tail].move = gen.p.NewMove(square, targets.pop())
@@ -47,16 +45,17 @@ func (gen *MoveGen) pawnCaptures(color int) *MoveGen {
 
 // Generates all pseudo-legal captures by pieces other than pawn.
 func (gen *MoveGen) pieceCaptures(color int) *MoveGen {
-	for _, kind := range [5]int{ KNIGHT, BISHOP, ROOK, QUEEN, KING } {
+        enemy := color^1
+        for _, kind := range [4]int{ KNIGHT, BISHOP, ROOK, QUEEN } {
 	        outposts := gen.p.outposts[Piece(kind|color)]
 	        for outposts != 0 {
 	                square := outposts.pop()
-	                targets := gen.p.targets[square] & gen.p.board[color^1]
-	                for targets != 0 {
-	                        gen.list[gen.tail].move = gen.p.NewMove(square, targets.pop())
-	                        gen.tail++
-	                }
+	                gen.movePiece(square, gen.p.targets[square] & gen.p.board[enemy])
 	        }
-	}
-	return gen
+        }
+        if king := gen.p.outposts[King(color)]; king != 0 {
+                square := king.pop()
+                gen.moveKing(square, gen.p.targets[square] & gen.p.board[enemy])
+        }
+        return gen
 }
