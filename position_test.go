98d83b621783e50461b5c29e499e477e32678c31
Michael Dvorkin <mike@dvorkin.net>
2014-02-21 18:58:57 -0800

    Fixed en-passant tests

--- position_test.go
+++ position_test.go
@@ -225,10 +225,10 @@ func TestPosition230(t *testing.T) { // 1. e4 d5 2. e5 f5 <-- Enpassant
         p = p.MakeMove(p.NewMove(E2, E4))
         p = p.MakeMove(p.NewMove(D7, D5))
         p = p.MakeMove(p.NewMove(E4, E5))
-        p = p.MakeMove(p.NewMove(F7, F5))
+        p = p.MakeMove(p.NewEnpassant(F7, F5))
 
-        expect(t, p.polyglot(), uint64(0x22A48B5A8E47FF78))     // <<<
-        expect(t, p.flags.enpassant, F6)                        // <<<
+        expect(t, p.polyglot(), uint64(0x22A48B5A8E47FF78))
+        expect(t, p.flags.enpassant, F6)
         expect(t, p.castles, uint8(0x0F))
 }
 
@@ -265,10 +265,10 @@ func TestPosition260(t *testing.T) { // 1. a2a4 b7b5 2. h2h4 b5b4 3. c2c4 <-- En
         p = p.MakeMove(p.NewMove(B7, B5))
         p = p.MakeMove(p.NewMove(H2, H4))
         p = p.MakeMove(p.NewMove(B5, B4))
-        p = p.MakeMove(p.NewMove(C2, C4))
+        p = p.MakeMove(p.NewEnpassant(C2, C4))
 
-        expect(t, p.polyglot(), uint64(0x3C8123EA7B067637))      // <<<
-        expect(t, p.flags.enpassant, C3)                         // <<<
+        expect(t, p.polyglot(), uint64(0x3C8123EA7B067637))
+        expect(t, p.flags.enpassant, C3)
         expect(t, p.castles, uint8(0x0F))
 }
 
@@ -278,11 +278,11 @@ func TestPosition270(t *testing.T) { // 1. a2a4 b7b5 2. h2h4 b5b4 3. c2c4 b4xc3 
         p = p.MakeMove(p.NewMove(B7, B5))
         p = p.MakeMove(p.NewMove(H2, H4))
         p = p.MakeMove(p.NewMove(B5, B4))
-        p = p.MakeMove(p.NewMove(C2, C4))
+        p = p.MakeMove(p.NewEnpassant(C2, C4))
         p = p.MakeMove(p.NewMove(B4, C3))
         p = p.MakeMove(p.NewMove(A1, A3))
 
-        expect(t, p.polyglot(), uint64(0x5C3F9B829B279560))      // <<<
+        expect(t, p.polyglot(), uint64(0x5C3F9B829B279560))
         expect(t, p.flags.enpassant, 0)
         expect(t, p.castles, castleKingside[White] | castleKingside[Black] | castleQueenside[Black])
 }
