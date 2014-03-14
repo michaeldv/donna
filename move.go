20ceb6313323b2a763fac52048ff981d618a3f4a
Michael Dvorkin <mike@dvorkin.net>
2014-03-13 20:44:13 -0700

    Copyright et al

--- move.go
+++ move.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2013 by Michael Dvorkin. All Rights Reserved.
+// Copyright (c) 2013-2014 by Michael Dvorkin. All Rights Reserved.
 // Use of this source code is governed by a MIT-style license that can
 // be found in the LICENSE file.
 
