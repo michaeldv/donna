8653ddf725a10ed886ef0f4389863a1ace2da93f
Michael Dvorkin <mike@dvorkin.net>
2014-03-31 23:07:08 -0700

    Game status

--- data_readonly.go
+++ data_readonly.go
@@ -21,6 +21,17 @@ const (
 )
 
 const (
+        InProgress = iota
+        WhiteWinning
+        BlackWinning
+        WhiteWon
+        BlackWon
+        Stalemate
+        Repetition
+        MaterialDraw
+)
+
+const (
 	A1 = iota
 	    B1; C1; D1; E1; F1; G1; H1
 	A2; B2; C2; D2; E2; F2; G2; H2
