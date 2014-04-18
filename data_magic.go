a8ef123239806f9c697731121dc2b0c3aa74ffc3
Michael Dvorkin <mike@dvorkin.net>
2014-04-17 21:49:31 -0700

    go fmt

--- data_magic.go
+++ data_magic.go
@@ -4,59 +4,59 @@
 
 package donna
 
-import()
+import ()
 
 type Magic struct {
-	mask   Bitmask
-	magic  Bitmask
+	mask  Bitmask
+	magic Bitmask
 }
 
 var (
-        bit              [64]Bitmask
-        kingMoves        [64]Bitmask
-        knightMoves      [64]Bitmask
-        pawnMoves        [2][64]Bitmask
+	bit              [64]Bitmask
+	kingMoves        [64]Bitmask
+	knightMoves      [64]Bitmask
+	pawnMoves        [2][64]Bitmask
 	rookMagicMoves   [64][4096]Bitmask
 	bishopMagicMoves [64][512]Bitmask
 
-        maskPassed       [2][64]Bitmask
-        maskInFront      [2][64]Bitmask
+	maskPassed  [2][64]Bitmask
+	maskInFront [2][64]Bitmask
 
-        // Complete file or rank mask if both squares reside on on the same file
-        // or rank.
-        maskStraight     [64][64]Bitmask
+	// Complete file or rank mask if both squares reside on on the same file
+	// or rank.
+	maskStraight [64][64]Bitmask
 
-        // Complete diagonal mask if both squares reside on on the same diagonal.
-        maskDiagonal     [64][64]Bitmask
+	// Complete diagonal mask if both squares reside on on the same diagonal.
+	maskDiagonal [64][64]Bitmask
 
-        // If a king on square [x] gets checked from square [y] it can evade the
-        // check from all squares except maskEvade[x][y]. For example, if white
-        // king on B2 gets checked by black bishop on G7 the king can't step back
-        // to A1 (despite not being attacked by black).
-        maskEvade        [64][64]Bitmask
+	// If a king on square [x] gets checked from square [y] it can evade the
+	// check from all squares except maskEvade[x][y]. For example, if white
+	// king on B2 gets checked by black bishop on G7 the king can't step back
+	// to A1 (despite not being attacked by black).
+	maskEvade [64][64]Bitmask
 
-        // If a king on square [x] gets checked from square [y] the check can be
-        // evaded by moving a piece to maskBlock[x][y]. For example, if white
-        // king on B2 gets checked by black bishop on G7 the check can be evaded
-        // by moving white piece onto C3-G7 diagonal (including capture on G7).
-        maskBlock        [64][64]Bitmask
+	// If a king on square [x] gets checked from square [y] the check can be
+	// evaded by moving a piece to maskBlock[x][y]. For example, if white
+	// king on B2 gets checked by black bishop on G7 the check can be evaded
+	// by moving white piece onto C3-G7 diagonal (including capture on G7).
+	maskBlock [64][64]Bitmask
 
-        // Bitmask to indicate pawn attacks for a square. For example, C3 is being
-        // attacked by white pawns on B2 and D2, and black pawns on B4 and D4.
-        maskPawn         [2][64]Bitmask
+	// Bitmask to indicate pawn attacks for a square. For example, C3 is being
+	// attacked by white pawns on B2 and D2, and black pawns on B4 and D4.
+	maskPawn [2][64]Bitmask
 
-        // Two arrays to simplify incremental polyglot hash computation.
-        hashCastle       [16]uint64
-        hashEnpassant    [8]uint64
+	// Two arrays to simplify incremental polyglot hash computation.
+	hashCastle    [16]uint64
+	hashEnpassant [8]uint64
 )
 
 func init() {
 	for square := A1; square <= H8; square++ {
-                bit[square] = Bitmask(1 << uint(square))
-        }
+		bit[square] = Bitmask(1 << uint(square))
+	}
 
 	for square := A1; square <= H8; square++ {
-                row, col := Coordinate(square)
+		row, col := Coordinate(square)
 
 		// Rooks.
 		mask := createRookMask(square)
@@ -69,97 +69,96 @@ func init() {
 
 		// Bishops.
 		mask = createBishopMask(square)
-                bits = uint(mask.count())
+		bits = uint(mask.count())
 		for i := 0; i < (1 << bits); i++ {
 			bitmask := indexedBitmask(i, mask)
 			index := (bitmask * bishopMagic[square].magic) >> 55
 			bishopMagicMoves[square][index] = createBishopAttacks(square, bitmask)
 		}
 
-                // Pawns.
-                if row >= 1 && row <= 7 {
-                        if col > 0 {
-                                pawnMoves[White][square].set(Square(row+1, col-1))
-                                pawnMoves[Black][square].set(Square(row-1, col-1))
-                        }
-                        if col < 7 {
-                                pawnMoves[White][square].set(Square(row+1, col+1))
-                                pawnMoves[Black][square].set(Square(row-1, col+1))
-                        }
-                }
+		// Pawns.
+		if row >= 1 && row <= 7 {
+			if col > 0 {
+				pawnMoves[White][square].set(Square(row + 1, col - 1))
+				pawnMoves[Black][square].set(Square(row - 1, col - 1))
+			}
+			if col < 7 {
+				pawnMoves[White][square].set(Square(row + 1, col + 1))
+				pawnMoves[Black][square].set(Square(row - 1, col + 1))
+			}
+		}
 
-                // Blocks, Evasions, Straight, Diagonals, Knights, and Kings.
-                for i := A1; i <= H8; i++ {
+		// Blocks, Evasions, Straight, Diagonals, Knights, and Kings.
+		for i := A1; i <= H8; i++ {
 			r, c := Coordinate(i)
 			setMasks(square, i, row, col, r, c)
 
-                        if i == square || Abs(i - square) > 17 {
-                                continue // No king or knight can reach that far.
-                        }
-                        if (Abs(r - row) == 2 && Abs(c - col) == 1) || (Abs(r - row) == 1 && Abs(c - col) == 2) {
-                                knightMoves[square].set(i)
-                        }
-                        if Abs(r - row) <= 1 && Abs(c - col) <= 1 {
-                                kingMoves[square].set(i)
-                        }
-                }
+			if i == square || Abs(i-square) > 17 {
+				continue // No king or knight can reach that far.
+			}
+			if (Abs(r-row) == 2 && Abs(c-col) == 1) || (Abs(r-row) == 1 && Abs(c-col) == 2) {
+				knightMoves[square].set(i)
+			}
+			if Abs(r-row) <= 1 && Abs(c-col) <= 1 {
+				kingMoves[square].set(i)
+			}
+		}
 
-                // Pawn attacks.
-                if row > 1 { // White pawns can't attack first two ranks.
-                        if col != 0 {
-                                maskPawn[White][square] |= bit[square - 9]
-                        }
-                        if col != 7 {
-                                maskPawn[White][square] |= bit[square - 7]
-                        }
-                }
-                if row < 6 { // Black pawns can attack 7th and 8th ranks.
-                        if col != 0 {
-                                maskPawn[Black][square] |= bit[square + 7]
-                        }
-                        if col != 7 {
-                                maskPawn[Black][square] |= bit[square + 9]
-                        }
-                }
+		// Pawn attacks.
+		if row > 1 { // White pawns can't attack first two ranks.
+			if col != 0 {
+				maskPawn[White][square] |= bit[square-9]
+			}
+			if col != 7 {
+				maskPawn[White][square] |= bit[square-7]
+			}
+		}
+		if row < 6 { // Black pawns can attack 7th and 8th ranks.
+			if col != 0 {
+				maskPawn[Black][square] |= bit[square+7]
+			}
+			if col != 7 {
+				maskPawn[Black][square] |= bit[square+9]
+			}
+		}
 
-                // Masks to check for passed pawns.
-                if col > 0 {
-                        maskPassed[White][square].fill(square - 1,  8, 0, 0x00FFFFFFFFFFFFFF)
-                        maskPassed[Black][square].fill(square - 1, -8, 0, 0xFFFFFFFFFFFFFF00)
-                }
-                maskPassed[White][square].fill(square,  8, 0, 0x00FFFFFFFFFFFFFF)
-                maskPassed[Black][square].fill(square, -8, 0, 0xFFFFFFFFFFFFFF00)
-                if col < 7 {
-                        maskPassed[White][square].fill(square + 1,  8, 0, 0x00FFFFFFFFFFFFFF)
-                        maskPassed[Black][square].fill(square + 1, -8, 0, 0xFFFFFFFFFFFFFF00)
-                }
+		// Masks to check for passed pawns.
+		if col > 0 {
+			maskPassed[White][square].fill(square-1, 8, 0, 0x00FFFFFFFFFFFFFF)
+			maskPassed[Black][square].fill(square-1, -8, 0, 0xFFFFFFFFFFFFFF00)
+		}
+		maskPassed[White][square].fill(square, 8, 0, 0x00FFFFFFFFFFFFFF)
+		maskPassed[Black][square].fill(square, -8, 0, 0xFFFFFFFFFFFFFF00)
+		if col < 7 {
+			maskPassed[White][square].fill(square+1, 8, 0, 0x00FFFFFFFFFFFFFF)
+			maskPassed[Black][square].fill(square+1, -8, 0, 0xFFFFFFFFFFFFFF00)
+		}
 
-                // Vertical squares in front of a pawn.
-                maskInFront[White][square].fill(square,  8, 0, 0x00FFFFFFFFFFFFFF)
-                maskInFront[Black][square].fill(square, -8, 0, 0xFFFFFFFFFFFFFF00)
+		// Vertical squares in front of a pawn.
+		maskInFront[White][square].fill(square, 8, 0, 0x00FFFFFFFFFFFFFF)
+		maskInFront[Black][square].fill(square, -8, 0, 0xFFFFFFFFFFFFFF00)
 	}
 
+	// Castle hash values.
+	for mask := uint8(0); mask < 16; mask++ {
+		if mask & castleKingside[White] != 0 {
+			hashCastle[mask] ^= polyglotRandomCastle[0]
+		}
+		if mask & castleQueenside[White] != 0 {
+			hashCastle[mask] ^= polyglotRandomCastle[1]
+		}
+		if mask & castleKingside[Black] != 0 {
+			hashCastle[mask] ^= polyglotRandomCastle[2]
+		}
+		if mask & castleQueenside[Black] != 0 {
+			hashCastle[mask] ^= polyglotRandomCastle[3]
+		}
+	}
 
-        // Castle hash values.
-        for mask := uint8(0); mask < 16; mask++ {
-                if mask & castleKingside[White] != 0 {
-                        hashCastle[mask] ^= polyglotRandomCastle[0]
-                }
-                if mask & castleQueenside[White] != 0 {
-                        hashCastle[mask] ^= polyglotRandomCastle[1]
-                }
-                if mask & castleKingside[Black] != 0 {
-                        hashCastle[mask] ^= polyglotRandomCastle[2]
-                }
-                if mask & castleQueenside[Black] != 0 {
-                        hashCastle[mask] ^= polyglotRandomCastle[3]
-                }
-        }
-
-        // Enpassant hash values.
-        for col := A1; col <= H1; col++ {
-                hashEnpassant[col] = polyglotRandomEnpassant[col]
-        }
+	// Enpassant hash values.
+	for col := A1; col <= H1; col++ {
+		hashEnpassant[col] = polyglotRandomEnpassant[col]
+	}
 }
 
 func indexedBitmask(index int, mask Bitmask) (bitmask Bitmask) {
@@ -224,7 +223,7 @@ func createRookAttacks(square int, mask Bitmask) (bitmask Bitmask) {
 
 	// North.
 	for c, r := col, row + 1; r <= 7; r++ {
-                bit := bit[r * 8 + c]
+		bit := bit[r * 8 + c]
 		bitmask |= bit
 		if mask & bit != 0 {
 			break
@@ -232,7 +231,7 @@ func createRookAttacks(square int, mask Bitmask) (bitmask Bitmask) {
 	}
 	// East.
 	for c, r := col + 1, row; c <= 7; c++ {
-                bit := bit[r * 8 + c]
+		bit := bit[r * 8 + c]
 		bitmask |= bit
 		if mask & bit != 0 {
 			break
@@ -240,7 +239,7 @@ func createRookAttacks(square int, mask Bitmask) (bitmask Bitmask) {
 	}
 	// South.
 	for c, r := col, row - 1; r >= 0; r-- {
-                bit := bit[r * 8 + c]
+		bit := bit[r * 8 + c]
 		bitmask |= bit
 		if mask & bit != 0 {
 			break
@@ -248,7 +247,7 @@ func createRookAttacks(square int, mask Bitmask) (bitmask Bitmask) {
 	}
 	// West
 	for c, r := col - 1, row; c >= 0; c-- {
-                bit := bit[r * 8 + c]
+		bit := bit[r * 8 + c]
 		bitmask |= bit
 		if mask & bit != 0 {
 			break
@@ -262,7 +261,7 @@ func createBishopAttacks(square int, mask Bitmask) (bitmask Bitmask) {
 
 	// North East.
 	for c, r := col + 1, row + 1; c <= 7 && r <= 7; c, r = c+1, r+1 {
-                bit := bit[r * 8 + c]
+		bit := bit[r * 8 + c]
 		bitmask |= bit
 		if mask & bit != 0 {
 			break
@@ -270,23 +269,23 @@ func createBishopAttacks(square int, mask Bitmask) (bitmask Bitmask) {
 	}
 	// South East.
 	for c, r := col + 1, row - 1; c <= 7 && r >= 0; c, r = c+1, r-1 {
-                bit := bit[r * 8 + c]
+		bit := bit[r * 8 + c]
 		bitmask |= bit
 		if mask & bit != 0 {
 			break
 		}
 	}
-        // South West.
+	// South West.
 	for c, r := col - 1, row - 1; c >= 0 && r >= 0; c, r = c-1, r-1 {
-                bit := bit[r * 8 + c]
+		bit := bit[r * 8 + c]
 		bitmask |= bit
 		if mask & bit != 0 {
 			break
 		}
 	}
-        // North West.
+	// North West.
 	for c, r := col - 1, row + 1; c >= 0 && r <= 7; c, r = c-1, r+1 {
-                bit := bit[r * 8 + c]
+		bit := bit[r * 8 + c]
 		bitmask |= bit
 		if mask & bit != 0 {
 			break
@@ -304,9 +303,9 @@ func setMasks(square, target, row, col, r, c int) {
 			maskBlock[square][target].fill(square, -1, bit[target], maskFull)
 			maskEvade[square][target] = ^((bit[square] & ^maskFile[7]) << 1)
 		}
-                if col != c {
-                        maskStraight[square][target] = maskRank[r]
-                }
+		if col != c {
+			maskStraight[square][target] = maskRank[r]
+		}
 	} else if col == c {
 		if row < r {
 			maskBlock[square][target].fill(square, 8, bit[target], maskFull)
@@ -315,10 +314,10 @@ func setMasks(square, target, row, col, r, c int) {
 			maskBlock[square][target].fill(square, -8, bit[target], maskFull)
 			maskEvade[square][target] = ^((bit[square] & ^maskRank[7]) << 8)
 		}
-                if row != r {
-                        maskStraight[square][target] = maskFile[c]
-                }
-	} else if r + col == row + c { // Diagonals (A1->H8).
+		if row != r {
+			maskStraight[square][target] = maskFile[c]
+		}
+	} else if r+col == row+c { // Diagonals (A1->H8).
 		if col < c {
 			maskBlock[square][target].fill(square, 9, bit[target], maskFull)
 			maskEvade[square][target] = ^((bit[square] & ^maskRank[0] & ^maskFile[0]) >> 9)
@@ -326,12 +325,12 @@ func setMasks(square, target, row, col, r, c int) {
 			maskBlock[square][target].fill(square, -9, bit[target], maskFull)
 			maskEvade[square][target] = ^((bit[square] & ^maskRank[7] & ^maskFile[7]) << 9)
 		}
-                if shift := (r - c) & 15; shift < 8 { // A1-A8-H8
-                        maskDiagonal[square][target] = maskA1H8 << uint(8 * shift)
-                } else { // B1-H1-H7
-                        maskDiagonal[square][target] = maskA1H8 >> uint(8 * (16-shift))
-                }
-	} else if row + col == r + c { // AntiDiagonals (H1->A8).
+		if shift := (r - c) & 15; shift < 8 { // A1-A8-H8
+			maskDiagonal[square][target] = maskA1H8 << uint(8*shift)
+		} else { // B1-H1-H7
+			maskDiagonal[square][target] = maskA1H8 >> uint(8*(16-shift))
+		}
+	} else if row+col == r+c { // AntiDiagonals (H1->A8).
 		if col < c {
 			maskBlock[square][target].fill(square, -7, bit[target], maskFull)
 			maskEvade[square][target] = ^((bit[square] & ^maskRank[7] & ^maskFile[0]) << 7)
@@ -339,11 +338,11 @@ func setMasks(square, target, row, col, r, c int) {
 			maskBlock[square][target].fill(square, 7, bit[target], maskFull)
 			maskEvade[square][target] = ^((bit[square] & ^maskRank[0] & ^maskFile[7]) >> 7)
 		}
-                if shift := 7 ^ (r + c); shift < 8 { // A8-A1-H1
-                        maskDiagonal[square][target] = maskH1A8 >> uint(8 * shift)
-                } else { // B8-H8-H2
-                        maskDiagonal[square][target] = maskH1A8 << uint(8 * (16-shift))
-                }
+		if shift := 7 ^ (r + c); shift < 8 { // A8-A1-H1
+			maskDiagonal[square][target] = maskH1A8 >> uint(8*shift)
+		} else { // B8-H8-H2
+			maskDiagonal[square][target] = maskH1A8 << uint(8*(16-shift))
+		}
 	}
 	//
 	// Default values are all 0 for maskBlock[square][target] (Go sets it for us)
--- data_magic_test.go
+++ data_magic_test.go
@@ -7,60 +7,60 @@ package donna
 import (`testing`)
 
 func TestMagic000(t *testing.T) {
-        expect(t, maskBlock[C3][H8], Bitmask( bit[D4] | bit[E5] | bit[F6] | bit[G7] | bit[H8] ))
-        expect(t, maskBlock[C3][C8], Bitmask( bit[C4] | bit[C5] | bit[C6] | bit[C7] | bit[C8] ))
-        expect(t, maskBlock[C3][A5], Bitmask( bit[B4] | bit[A5]                               ))
-        expect(t, maskBlock[C3][A3], Bitmask( bit[B3] | bit[A3]                               ))
-        expect(t, maskBlock[C3][A1], Bitmask( bit[B2] | bit[A1]                               ))
-        expect(t, maskBlock[C3][C1], Bitmask( bit[C2] | bit[C1]                               ))
-        expect(t, maskBlock[C3][E1], Bitmask( bit[D2] | bit[E1]                               ))
-        expect(t, maskBlock[C3][H3], Bitmask( bit[D3] | bit[E3] | bit[F3] | bit[G3] | bit[H3] ))
-        expect(t, maskBlock[C3][E7], Bitmask(0))
+	expect(t, maskBlock[C3][H8], Bitmask( bit[D4] | bit[E5] | bit[F6] | bit[G7] | bit[H8] ))
+	expect(t, maskBlock[C3][C8], Bitmask( bit[C4] | bit[C5] | bit[C6] | bit[C7] | bit[C8] ))
+	expect(t, maskBlock[C3][A5], Bitmask( bit[B4] | bit[A5]                               ))
+	expect(t, maskBlock[C3][A3], Bitmask( bit[B3] | bit[A3]                               ))
+	expect(t, maskBlock[C3][A1], Bitmask( bit[B2] | bit[A1]                               ))
+	expect(t, maskBlock[C3][C1], Bitmask( bit[C2] | bit[C1]                               ))
+	expect(t, maskBlock[C3][E1], Bitmask( bit[D2] | bit[E1]                               ))
+	expect(t, maskBlock[C3][H3], Bitmask( bit[D3] | bit[E3] | bit[F3] | bit[G3] | bit[H3] ))
+	expect(t, maskBlock[C3][E7], Bitmask(0))
 }
 
 func TestMagic010(t *testing.T) {
-        expect(t, maskEvade[C3][H8], Bitmask( ^bit[B2] ))
-        expect(t, maskEvade[C3][C8], Bitmask( ^bit[C2] ))
-        expect(t, maskEvade[C3][A5], Bitmask( ^bit[D2] ))
-        expect(t, maskEvade[C3][A3], Bitmask( ^bit[D3] ))
-        expect(t, maskEvade[C3][A1], Bitmask( ^bit[D4] ))
-        expect(t, maskEvade[C3][C1], Bitmask( ^bit[C4] ))
-        expect(t, maskEvade[C3][E1], Bitmask( ^bit[B4] ))
-        expect(t, maskEvade[C3][H3], Bitmask( ^bit[B3] ))
-        expect(t, maskEvade[C3][E7], Bitmask(maskFull))
+	expect(t, maskEvade[C3][H8], Bitmask( ^bit[B2] ))
+	expect(t, maskEvade[C3][C8], Bitmask( ^bit[C2] ))
+	expect(t, maskEvade[C3][A5], Bitmask( ^bit[D2] ))
+	expect(t, maskEvade[C3][A3], Bitmask( ^bit[D3] ))
+	expect(t, maskEvade[C3][A1], Bitmask( ^bit[D4] ))
+	expect(t, maskEvade[C3][C1], Bitmask( ^bit[C4] ))
+	expect(t, maskEvade[C3][E1], Bitmask( ^bit[B4] ))
+	expect(t, maskEvade[C3][H3], Bitmask( ^bit[B3] ))
+	expect(t, maskEvade[C3][E7], Bitmask(maskFull))
 }
 
 func TestMagic020(t *testing.T) {
-        expect(t, maskPawn[White][A3], Bitmask( bit[B2] ))
-        expect(t, maskPawn[White][D5], Bitmask( bit[C4] | bit[E4] ))
-        expect(t, maskPawn[White][F8], Bitmask( bit[E7] | bit[G7] ))
-        expect(t, maskPawn[Black][H4], Bitmask( bit[G5] ))
-        expect(t, maskPawn[Black][C5], Bitmask( bit[B6] | bit[D6] ))
-        expect(t, maskPawn[Black][B1], Bitmask( bit[A2] | bit[C2] ))
+	expect(t, maskPawn[White][A3], Bitmask( bit[B2] ))
+	expect(t, maskPawn[White][D5], Bitmask( bit[C4] | bit[E4] ))
+	expect(t, maskPawn[White][F8], Bitmask( bit[E7] | bit[G7] ))
+	expect(t, maskPawn[Black][H4], Bitmask( bit[G5] ))
+	expect(t, maskPawn[Black][C5], Bitmask( bit[B6] | bit[D6] ))
+	expect(t, maskPawn[Black][B1], Bitmask( bit[A2] | bit[C2] ))
 }
 
 func TestMagic030(t *testing.T) {
-        // Same file.
-        expect(t, maskStraight[A2][A5], maskFile[0])
-        expect(t, maskStraight[H6][H1], maskFile[7])
-        // Same rank.
-        expect(t, maskStraight[A2][F2], maskRank[1])
-        expect(t, maskStraight[H6][B6], maskRank[5])
-        // Edge cases.
-        expect(t, maskStraight[A1][C5], maskNone) // Random squares.
-        expect(t, maskStraight[E4][E4], maskNone) // Same square.
+	// Same file.
+	expect(t, maskStraight[A2][A5], maskFile[0])
+	expect(t, maskStraight[H6][H1], maskFile[7])
+	// Same rank.
+	expect(t, maskStraight[A2][F2], maskRank[1])
+	expect(t, maskStraight[H6][B6], maskRank[5])
+	// Edge cases.
+	expect(t, maskStraight[A1][C5], maskNone) // Random squares.
+	expect(t, maskStraight[E4][E4], maskNone) // Same square.
 }
 
 func TestMagic040(t *testing.T) {
-        // Same diagonal.
-        expect(t, maskDiagonal[C4][F7], bit[A2] | bit[B3] | bit[C4] | bit[D5] | bit[E6] | bit[F7] | bit[G8])
-        expect(t, maskDiagonal[F6][H8], maskA1H8)
-        expect(t, maskDiagonal[F1][H3], bit[F1] | bit[G2] | bit[H3])
-        // Same anti-diagonal.
-        expect(t, maskDiagonal[C2][B3], bit[D1] | bit[C2] | bit[B3] | bit[A4])
-        expect(t, maskDiagonal[F3][B7], maskH1A8)
-        expect(t, maskDiagonal[H3][D7], bit[H3] | bit[G4] | bit[F5] | bit[E6] | bit[D7] | bit[C8])
-        // Edge cases.
-        expect(t, maskDiagonal[A2][G4], maskNone) // Random squares.
-        expect(t, maskDiagonal[E4][E4], maskNone) // Same square.
+	// Same diagonal.
+	expect(t, maskDiagonal[C4][F7], bit[A2] | bit[B3] | bit[C4] | bit[D5] | bit[E6] | bit[F7] | bit[G8])
+	expect(t, maskDiagonal[F6][H8], maskA1H8)
+	expect(t, maskDiagonal[F1][H3], bit[F1] | bit[G2] | bit[H3])
+	// Same anti-diagonal.
+	expect(t, maskDiagonal[C2][B3], bit[D1] | bit[C2] | bit[B3] | bit[A4])
+	expect(t, maskDiagonal[F3][B7], maskH1A8)
+	expect(t, maskDiagonal[H3][D7], bit[H3] | bit[G4] | bit[F5] | bit[E6] | bit[D7] | bit[C8])
+	// Edge cases.
+	expect(t, maskDiagonal[A2][G4], maskNone) // Random squares.
+	expect(t, maskDiagonal[E4][E4], maskNone) // Same square.
 }
