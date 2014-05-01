2e3ea13a052e2f257f57db3a0a715ea98e045021
Michael Dvorkin <mike@dvorkin.net>
2014-04-30 22:11:21 -0700

    King threats

--- evaluate.go
+++ evaluate.go
@@ -13,15 +13,17 @@ type Evaluator struct {
 	stage    int
 	midgame  int
 	endgame  int
+	attacks  [2]int
+	threats  [2]int
 	position *Position
 }
 
 func (p *Position) Evaluate() int {
-	evaluator := &Evaluator{0, 0, 0, p}
+	evaluator := &Evaluator{0, 0, 0, [2]int{0, 0}, [2]int{0, 0}, p}
 	evaluator.analyzeMaterial()
-	evaluator.analyzeCoordination()
-	evaluator.analyzePawnStructure()
-	evaluator.analyzeKingSafety()
+	evaluator.analyzePieces()
+	evaluator.analyzePawns()
+	evaluator.analyzeSafety()
 
 	if p.color == White {
 		evaluator.midgame += rightToMove.midgame
@@ -31,6 +33,16 @@ func (p *Position) Evaluate() int {
 	evaluator.midgame -= rightToMove.midgame
 	evaluator.endgame -= rightToMove.endgame
 	return p.score(-evaluator.midgame, -evaluator.endgame)
+}
+
+func (e *Evaluator) analyzeMaterial() {
+	counters := &e.position.count
+	for _, piece := range []Piece{Pawn, Knight, Bishop, Rook, Queen} {
+		count := counters[piece] - counters[piece|Black]
+		midgame, endgame := piece.value()
+		e.midgame += midgame * count
+		e.endgame += endgame * count
+	}
 }
 
 func (e *Evaluator) strongEnough(color int) bool {
--- evaluate_safety.go
+++ evaluate_safety.go
@@ -4,45 +4,56 @@
 
 package donna
 
-func (e *Evaluator) analyzeKingSafety() {
+func (e *Evaluator) analyzeSafety() {
+	var white, black Score
 
-	// Placement.
+	// White king's board position and enemy threats.
 	square := e.position.king[White] ^ 56
-	e.midgame += bonusKing[0][square]
-	e.endgame += bonusKing[1][square]
-	square = e.position.king[Black]
-	e.midgame -= bonusKing[0][square]
-	e.endgame -= bonusKing[1][square]
+	white.midgame += bonusKing[0][square]
+	white.endgame += bonusKing[1][square]
+	if e.attacks[Black] > 0 {
+		white.midgame -= Max(3, e.attacks[Black]) * e.threats[Black]
+		white.endgame -= bonusKing[1][square]
+	}
 
-	// No endgame bonus or penalty.
+	// Black king's board position and enemy threats.
+	square = e.position.king[Black]
+	black.midgame += bonusKing[0][square]
+	black.endgame += bonusKing[1][square]
+	if e.attacks[White] > 0 {
+		e.midgame -= Max(3, e.attacks[White]) * e.threats[White]
+		e.endgame -= bonusKing[1][square]
+	}
+
+	e.midgame += white.midgame - black.midgame
+	e.endgame += white.endgame - black.endgame
+
+	// No endgame bonus or penalty for king shield.
 	e.midgame += e.kingShieldScore(White) - e.kingShieldScore(Black)
 }
 
 func (e *Evaluator) kingShieldScore(color int) (penalty int) {
 	p := e.position
 	kings, pawns := p.outposts[king(color)], p.outposts[pawn(color)]
-	//
+
 	// Pass if a) the king is missing, b) the king is on the initial square
 	// or c) the opposite side doesn't have a queen with one major piece.
-	//
 	if kings == 0 || kings == bit[homeKing[color]] || !e.strongEnough(color^1) {
 		return
 	}
-	//
+
 	// Calculate relative square for the king so we could treat black king
 	// as white. Don't bother with the shield if the king is too far.
-	//
 	square := Flip(color^1, p.king[color])
 	if square > H3 {
 		return
 	}
 	row, col := Coordinate(square)
 	from, to := Max(0, col-1), Min(7, col+1)
-	//
+
 	// For each of the shield columns find the closest same color pawn. The
 	// penalty is carried if the pawn is missing or is too far from the king
 	// (more than one row apart).
-	//
 	for column := from; column <= to; column++ {
 		if shield := (pawns & maskFile[column]); shield != 0 {
 			closest := Flip(color^1, shield.first()) // Make it relative.
