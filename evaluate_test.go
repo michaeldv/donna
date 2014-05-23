457fc64c0d5348ceaebe1165a11eb9604d00c5ef
Michael Dvorkin <mike@dvorkin.net>
2014-05-22 21:52:14 -0700

    Updated evaluation tests

--- evaluate_test.go
+++ evaluate_test.go
@@ -36,7 +36,7 @@ func TestEvaluate030(t *testing.T) {
 	p := NewGame().Setup(`Ra1,Nb1,Bc1,Qd1,Ke1,Bf1,Nf3,Rh1,a2,b2,c2,d2,e4,f2,g2,h2`,
 		`Ra8,Nb8,Bc8,Qd8,Ke8,Bf8,Ng8,Rh8,a7,b7,c7,d7,e5,f7,g7,h7`).Start(Black)
 	score := p.Evaluate()
-	expect(t, score, -49) // +49 for White.
+	expect(t, score, -62)
 }
 
 // After 1. e2-e4 e7-e5 2. Ng1-f3 Ng8-f6
@@ -44,7 +44,7 @@ func TestEvaluate040(t *testing.T) {
 	p := NewGame().Setup(`Ra1,Nb1,Bc1,Qd1,Ke1,Bf1,Nf3,Rh1,a2,b2,c2,d2,e4,f2,g2,h2`,
 		`Ra8,Nb8,Bc8,Qd8,Ke8,Bf8,Nf6,Rh8,a7,b7,c7,d7,e5,f7,g7,h7`).Start(White)
 	score := p.Evaluate()
-	expect(t, score, rightToMove.blended(p.phase())) // Right to move only.
+	expect(t, score, 22)
 }
 
 // After 1. e2-e4 e7-e5 2. Ng1-f3 Nb8-c6
@@ -52,7 +52,7 @@ func TestEvaluate050(t *testing.T) {
 	p := NewGame().Setup(`Ra1,Nb1,Bc1,Qd1,Ke1,Bf1,Nf3,Rh1,a2,b2,c2,d2,e4,f2,g2,h2`,
 		`Ra8,Nc6,Bc8,Qd8,Ke8,Bf8,Ng8,Rh8,a7,b7,c7,d7,e5,f7,g7,h7`).Start(White)
 	score := p.Evaluate()
-	expect(t, score, -3) // +3 for White.
+	expect(t, score, 0)
 }
 
 // After 1. e2-e4 e7-e5 2. Ng1-f3 Nb8-c6 3. Nb1-c3 Ng8-f6
@@ -61,14 +61,4 @@ func TestEvaluate060(t *testing.T) {
 		`Ra8,Nc6,Bc8,Qd8,Ke8,Bf8,Nf6,Rh8,a7,b7,c7,d7,e5,f7,g7,h7`).Start(White)
 	score := p.Evaluate()
 	expect(t, score, rightToMove.blended(p.phase())) // Right to move only.
-}
-
-func TestEvaluate999(t *testing.T) {
-	p := NewGame().Setup(`Ke2,Rg7,Be6,c3,g3,h2`,
-		             `Kb8,Bb7,h6,e5,e4,b5,a5`).Start(Black)
-	LogOn(); defer LogOff()
-	Lop(p)
-	Lop(p.material())
-	score := p.Evaluate()
-	expect(t, score, 1)
 }
