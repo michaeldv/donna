ec5820f10aec8350afd9a298bd1fffac8dc3339b
Michael Dvorkin <mike@dvorkin.net>
2014-06-04 22:58:36 -0700

    Bumped up default depth in REPL

--- cmd/donna.go
+++ cmd/donna.go
@@ -17,13 +17,13 @@ func repl() {
 	setup := func() {
 		if game == nil || position == nil {
 			game = donna.NewGame().InitialPosition()
-			position = game.CacheSize(32).Start(donna.White)
+			position = game.CacheSize(64).Start(donna.White)
 			fmt.Printf("%s\n", position)
 		}
 	}
 
 	think := func() {
-		if move := game.Think(9, position); move != 0 {
+		if move := game.Think(10, position); move != 0 {
 			position = position.MakeMove(move)
 			fmt.Printf("%s\n", position)
 		}
@@ -81,12 +81,6 @@ func main() {
 }
 
 func benchmark() {
-	// Tylkowski vs. Wojciechowski, Poznan 1931
-	// 30...Rxb2! 31. Nxb2 c3 32. Rxb6 c4!! 33. Rb4 a5! 34.Na4 axb4
-	// game := donna.NewGame().Setup(`Kg1,Rb7,Na4,a2,b2,f4,g2,g3`, `Kh7,Rd2,Bb6,a7,c5,c4,g7,h6`)
-	// fmt.Printf("%s\n", game)
-	// game.CacheSize(64).Think(11, game.Start(donna.Black))
-
 	// Sergey Kaminer, 1935
 	// 1.h8Q+ Kxh8 2.Ng4+ Kg7 3.Qh7+ Kf8 4.Qg8+ Ke7 5.Qe8+ Kd6 6.Qd7+ Kc5 7.Qb5+ Kd6
 	// 8.Qb6+ Ke7 9.Qc7+ Kf8 10.Qc8+ Ke7 11.Qd7+ Kf8 12.Qe8+ Kg7 13.Qg8+ Kxg8 14.Kxf6+
@@ -94,31 +88,37 @@ func benchmark() {
 	fmt.Printf("%s\n", game)
 	game.CacheSize(64).Think(8, game.Start(donna.White))
 
+	// Tylkowski vs. Wojciechowski, Poznan 1931
+	// 30...Rxb2! 31. Nxb2 c3 32. Rxb6 c4!! 33. Rb4 a5! 34.Na4 axb4
+	game = donna.NewGame().Setup(`Kg1,Rb7,Na4,a2,b2,f4,g2,g3`, `Kh7,Rd2,Bb6,a7,c5,c4,g7,h6`)
+	fmt.Printf("%s\n", game)
+	game.CacheSize(64).Think(12, game.Start(donna.Black))
+
 	// Georg Rotlewi vs. Akiba Rubinstein, Lodz 1907, after 21 moves.
 	// http://www.chessgames.com/perl/chessgame?gid=1119679
 	game = donna.NewGame().Setup(`Kh1,Qe2,Ra1,Rf1,Bb2,Be4,Nc3,a3,b4,e5,f4,g2,h2`, `Kg8,Qe7,Rc8,Rd8,Bb6,Bb7,Ng4,a6,b5,e6,f7,g7,h7`)
 	fmt.Printf("%s\n", game)
-	game.CacheSize(64).Think(9, game.Start(donna.Black))
+	game.CacheSize(64).Think(12, game.Start(donna.Black))
 
 	// Donald Byrne vs. Bobby Fischer, Third Rosenwald Trophy, 1956 after 16 moves.
 	// http://www.chessgames.com/perl/chessgame?gid=1008361
 	// 16... Rf8-e8 17. Ke1-f1 Bg4-e6!!
 	game = donna.NewGame().Setup(`Ke1,Qa3,Rd1,Rh1,Bc4,Bc5,Nf3,a2,d4,f2,g2,h2`, `Kg8,Qb6,Ra8,Rf8,Bg4,Bg7,Nc3,a7,b7,c6,f7,g6,h7`)
 	fmt.Printf("%s\n", game)
-	game.CacheSize(64).Think(9, game.Start(donna.Black))
+	game.CacheSize(64).Think(12, game.Start(donna.Black))
 
 	// Bobby Fischer vs. James Sherwin, New Jersey Open 1957, after 16 moves.
 	// http://www.chessgames.com/perl/chessgame?gid=1008366
 	// Fischer played 17. h2-h4!
 	game = donna.NewGame().Setup(`Kg1,Qc2,Ra1,Re1,Bc1,Bg2,Ng5,a2,b2,c3,d4,f2,g3,h2`, `Kg8,Qd6,Ra8,Rf8,Bc8,Nd5,Ng6,a7,b6,c4,e6,f7,g7,h7`)
 	fmt.Printf("%s\n", game)
-	game.CacheSize(64).Think(9, game.Start(donna.White))
+	game.CacheSize(64).Think(10, game.Start(donna.White))
 
 	// Mikhail Botvinnik vs. Jose Raul Capablanca, AVRO 1936, after 29 moves.
 	// Botvinnik played 30. Bb2-a3!
 	game = donna.NewGame().Setup(`Kg1,Qe5,Bb2,Ng3,c3,d4,e6,g2,h2`, `Kg7,Qe7,Nb3,Nf6,a7,b6,c4,d5,g6,h7`)
 	fmt.Printf("%s\n", game)
-	game.CacheSize(64).Think(9, game.Start(donna.White))
+	game.CacheSize(64).Think(12, game.Start(donna.White))
 
 }
 
