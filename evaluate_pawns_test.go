c4f3f9ed348ae9f0333e068b2be38d5f795785a4
Michael Dvorkin <mike@dvorkin.net>
2014-06-25 21:32:34 -0700

    Updated evaluation tests

--- evaluate_pawns_test.go
+++ evaluate_pawns_test.go
@@ -17,14 +17,14 @@ func TestEvaluatePawns110(t *testing.T) {
 	game := NewGame().Setup(`Ke1,h2,h3`, `Ke8,a7,h7`)
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, -18)
+	expect(t, score, -15)
 }
 
 func TestEvaluatePawns120(t *testing.T) {
 	game := NewGame().Setup(`Ke1,f4,f5`, `Ke8,f7,h7`)
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, -18)
+	expect(t, score, -14)
 }
 
 // Passed pawns.
@@ -39,7 +39,7 @@ func TestEvaluatePawns210(t *testing.T) {
 	game := NewGame().Setup(`Ke1,h4`, `Ke8,g7`) // Can't pass.
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 11)
+	expect(t, score, 10)
 }
 
 func TestEvaluatePawns220(t *testing.T) {
@@ -60,14 +60,14 @@ func TestEvaluatePawns240(t *testing.T) {
 	game := NewGame().Setup(`Kd1,e5`, `Ke8,d5`) // Both passing but white is closer.
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 36)
+	expect(t, score, 37)
 }
 
 func TestEvaluatePawns250(t *testing.T) {
 	game := NewGame().Setup(`Ke1,a5`, `Kd8,h7`) // Both passing but white is much closer.
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 70)
+	expect(t, score, 106)
 }
 
 // Isolated pawns.
@@ -75,14 +75,14 @@ func TestEvaluatePawns300(t *testing.T) {
 	game := NewGame().Setup(`Ke1,a5,c5`, `Kd8,f4,h4`) // All pawns are isolated.
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 17)
+	expect(t, score, 5)
 }
 
 func TestEvaluatePawns310(t *testing.T) {
 	game := NewGame().Setup(`Ke1,a2,c2,e2`, `Ke8,a7,b7,c7`) // White pawns are isolated.
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, -52)
+	expect(t, score, -38)
 }
 
 // Rooks.
@@ -97,14 +97,14 @@ func TestEvaluatePawns410(t *testing.T) {
 	game := NewGame().Setup(`Ke1,Rb1,Ng2,a2`, `Ke8,Rh8,Nb7,h7`) // White on open file.
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 112)
+	expect(t, score, 125)
 }
 
 func TestEvaluatePawns420(t *testing.T) {
 	game := NewGame().Setup(`Ke1,Rb1,a2,g2`, `Ke8,Rh8,h7,b7`) // White on semi-open file.
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 119)
+	expect(t, score, 135)
 }
 
 // King shield.
@@ -118,49 +118,49 @@ func TestEvaluatePawns505(t *testing.T) {
 	game := NewGame().Setup(`Kg1,f2,g2,h2,Qa3,Na4`, `Kg8,f7,g6,h7,Qa6,Na5`) // h2,g2,h2 vs f7,G6,h7
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 13)
+	expect(t, score, 15)
 }
 
 func TestEvaluatePawns510(t *testing.T) {
 	game := NewGame().Setup(`Kg1,f2,g2,h2,Qa3,Na4`, `Kg8,f5,g6,h7,Qa6,Na5`) // h2,g2,h2 vs F5,G6,h7
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 22)
+	expect(t, score, 28)
 }
 
 func TestEvaluatePawns520(t *testing.T) {
 	game := NewGame().Setup(`Kg1,f2,g2,h2,Qa3,Na4`, `Kg8,a7,f7,g7,Qa6,Na5`) // h2,g2,h2 vs A7,f7,g7
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 45)
+	expect(t, score, 43)
 }
 
 func TestEvaluatePawns530(t *testing.T) {
 	game := NewGame().Setup(`Kb1,a3,b2,c2,Qh3,Nh4`, `Kb8,a7,b7,c7,Qh6,Nh5`) // A3,b2,c2 vs a7,b7,c7
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 2)
+	expect(t, score, 3)
 }
 
 func TestEvaluatePawns540(t *testing.T) {
 	game := NewGame().Setup(`Kb1,a3,b4,c2,Qh3,Nh4`, `Kb8,a7,b7,c7,Qh6,Nh5`) // A3,B4,c2 vs a7,b7,c7
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, -3)
+	expect(t, score, -4)
 }
 
 func TestEvaluatePawns550(t *testing.T) {
 	game := NewGame().Setup(`Kb1,b2,c2,h2,Qh3,Nh4`, `Kb8,a7,b7,c7,Qh6,Nh5`) // b2,c2,H2 vs a7,b7,c7
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, -29)
+	expect(t, score, -23)
 }
 
 func TestEvaluatePawns560(t *testing.T) {
 	game := NewGame().Setup(`Ka1,a3,b2,Qc1,Nd2`, `Kh8,g7,h6,Qf8,Ne7`) // a3,b2 == g7,h6
 	score := game.Start(White).Evaluate()
 
-	expect(t, score, 8)
+	expect(t, score, 9)
 }
 
 func TestEvaluatePawns570(t *testing.T) {
--- evaluate_threats_test.go
+++ evaluate_threats_test.go
@@ -9,43 +9,43 @@ import `testing`
 // Attacks by minor piece.
 func TestEvaluateThreats000(t *testing.T) {
 	// Baseline: bishop defended by pawn.
-	p := NewGame().Setup(`Kh1,Ne4`, `Kg7,Bf6,e7`).Start(White)
-	baseline, _ := p.EvaluateTest(`Threats`)
+	NewGame().Setup(`Kh1,Ne4`, `Kg7,Bf6,e7`).Start(White).EvaluateWithTrace()
+	baseline := eval.metrics[`Threats`].(Total).white
 
 	// Bishop not defended by pawn.
-	p = NewGame().Setup(`Kh1,Ne4,a2`, `Ke7,Bf6,a7`).Start(White)
-	score, _ := p.EvaluateTest(`Threats`)
+	NewGame().Setup(`Kh1,Ne4,a2`, `Ke7,Bf6,a7`).Start(White).EvaluateWithTrace()
+	score := eval.metrics[`Threats`].(Total).white
 	expect(t, score.minus(baseline), bonusMinorThreat[Bishop/2])
 
 	// Bishop and rook not defended by pawn (rook is stronger).
-	p = NewGame().Setup(`Kh1,Ne4,a2`, `Ke7,Bf6,Rd6,a7`).Start(White)
-	score, _ = p.EvaluateTest(`Threats`)
+	NewGame().Setup(`Kh1,Ne4,a2`, `Ke7,Bf6,Rd6,a7`).Start(White).EvaluateWithTrace()
+	score = eval.metrics[`Threats`].(Total).white
 	expect(t, score.minus(baseline), bonusMinorThreat[Rook/2])
 
 	// Hanging bishop with extra bonus for the right to move.
-	p = NewGame().Setup(`Kh1,Ne4,a2`, `Ka8,Bf6,a7`).Start(White)
-	score, _ = p.EvaluateTest(`Threats`)
+	NewGame().Setup(`Kh1,Ne4,a2`, `Ka8,Bf6,a7`).Start(White).EvaluateWithTrace()
+	score = eval.metrics[`Threats`].(Total).white
 	expect(t, score.minus(baseline), bonusMinorThreat[Bishop/2].plus(hangingAttack.times(2)))
 }
 
 // Attacks by major piece.
 func TestEvaluateThreats010(t *testing.T) {
 	// Baseline: bishop defended by pawn.
-	p := NewGame().Setup(`Kh1,Rf1`, `Kg7,Bf6,e7`).Start(White)
-	baseline, _ := p.EvaluateTest(`Threats`)
+	NewGame().Setup(`Kh1,Rf1`, `Kg7,Bf6,e7`).Start(White).EvaluateWithTrace()
+	baseline := eval.metrics[`Threats`].(Total).white
 
 	// Bishop not defended by pawn.
-	p = NewGame().Setup(`Kh1,Rf1`, `Ke7,Bf6`).Start(White)
-	score, _ := p.EvaluateTest(`Threats`)
+	NewGame().Setup(`Kh1,Rf1`, `Ke7,Bf6`).Start(White).EvaluateWithTrace()
+	score := eval.metrics[`Threats`].(Total).white
 	expect(t, score.minus(baseline), bonusMajorThreat[Bishop/2])
 
 	// Bishop and queen not defended by pawn (queen is stronger).
-	p = NewGame().Setup(`Kh1,Rf1`, `Ke7,Qa1,Bf6`).Start(White)
-	score, _ = p.EvaluateTest(`Threats`)
+	NewGame().Setup(`Kh1,Rf1`, `Ke7,Qa1,Bf6`).Start(White).EvaluateWithTrace()
+	score = eval.metrics[`Threats`].(Total).white
 	expect(t, score.minus(baseline), bonusMajorThreat[Queen/2])
 
 	// Hanging bishop with extra bonus for the right to move.
-	p = NewGame().Setup(`Kh1,Rf1`, `Kh8,Bf6`).Start(White)
-	score, _ = p.EvaluateTest(`Threats`)
+	NewGame().Setup(`Kh1,Rf1`, `Kh8,Bf6`).Start(White).EvaluateWithTrace()
+	score = eval.metrics[`Threats`].(Total).white
 	expect(t, score.minus(baseline), bonusMajorThreat[Bishop/2].plus(hangingAttack.times(2)))
 }
