af16aed2100e1aaa59ba172d14cd039d5f67ab49
Michael Dvorkin <mike@dvorkin.net>
2014-06-28 12:03:54 -0700

    Parse FEN to build a position

--- position_fen.go
+++ position_fen.go
@@ -5,6 +5,7 @@
 package donna
 
 import `regexp`
+// import `fmt`
 
 func NewPositionFromFEN(game *Game, fen string) *Position {
 	const ( 
@@ -33,6 +34,85 @@ func NewPositionFromFEN(game *Game, fen string) *Position {
 	matches := re.FindStringSubmatch(fen)
 	if matches == nil {
 		return nil
+	}
+	// fmt.Println(spx + row1 + rows26 + row8 + sp + color + sp + castle + sp + enpass + sp + number + sp + number + spx)
+	// fmt.Printf("%q\n", matches)
+	// fmt.Printf("%s\n", matches[1])
+
+	// [1] - Pieces (entire board).
+	square := A8
+	for _, char := range(matches[1]) {
+		piece := Piece(0)
+		// fmt.Printf("%c ", char)
+		switch(char) {
+		case 'P':
+			piece = Pawn
+		case 'p':
+			piece = BlackPawn
+		case 'N':
+			piece = Knight
+		case 'n':
+			piece = BlackKnight
+		case 'B':
+			piece = Bishop
+		case 'b':
+			piece = BlackBishop
+		case 'R':
+			piece = Rook
+		case 'r':
+			piece = BlackRook
+		case 'Q':
+			piece = Queen
+		case 'q':
+			piece = BlackQueen
+		case 'K':
+			piece = King
+			p.king[White] = square
+		case 'k':
+			piece = BlackKing
+			p.king[Black] = square
+		case '/':
+			square -= 16
+		case '1', '2', '3', '4', '5', '6', '7', '8':
+			square += int(char - '0')
+		}
+		if piece != 0 {
+			// fmt.Printf("%02d: %s\n", square, piece)
+			p.pieces[square] = piece
+			p.outposts[piece].set(square)
+			p.outposts[piece.color()].set(square)
+			p.count[piece]++
+			square++
+		}
+	}
+
+	// [3] - Color of side to move.
+	if matches[3] == `w` {
+		p.color = White
+	} else {
+		p.color = Black
+	}
+
+	// [4] - Castle rights.
+	for _, char := range(matches[4]) {
+		switch(char) {
+		case 'K':
+			p.castles |= castleKingside[White]
+		case 'Q':
+			p.castles |= castleQueenside[White]
+		case 'k':
+			p.castles |= castleKingside[Black]
+		case 'q':
+			p.castles |= castleQueenside[Black]
+		case '-':
+			// No castling rights.
+		}
+	}
+
+	// [5] - En-passant square.
+	if matches[5] != `-` {
+		p.enpassant = Square(int(matches[5][1] - '1'), int(matches[5][0] - 'a'))
+
 	}
 
 	p.reversible = true
