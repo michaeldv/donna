d72512ccdaab451036291aa76da3180addc508ee
Michael Dvorkin <mike@dvorkin.net>
2014-07-20 20:04:39 -0700

    Don't validate moves in root search since it's done by root move generator

--- search.go
+++ search.go
@@ -15,47 +15,46 @@ func (p *Position) search(alpha, beta, depth int) (score int) {
 
 	moveCount, bestMove := 0, Move(0)
 	for move := gen.NextMove(); move != 0; move = gen.NextMove() {
-		if position := p.MakeMove(move); position != nil {
-			moveCount++
-			newDepth := depth - 1
+		position := p.MakeMove(move)
+		moveCount++
 
-			// Search depth extension.
-			if position.isInCheck(p.color^1) { // Give check.
-				newDepth++
-			}
+		// Search depth extension.
+		newDepth := depth - 1
+		if position.isInCheck(p.color^1) { // Give check.
+			newDepth++
+		}
 
-			if moveCount == 1 {
+		if moveCount == 1 {
+			score = -position.searchTree(-beta, -alpha, newDepth)
+		} else {
+			score = -position.searchTree(-alpha - 1, -alpha, newDepth)
+			if score > alpha && score < beta {
 				score = -position.searchTree(-beta, -alpha, newDepth)
-			} else {
-				score = -position.searchTree(-alpha - 1, -alpha, newDepth)
-				if score > alpha && score < beta {
-					score = -position.searchTree(-beta, -alpha, newDepth)
-				}
 			}
-			position.TakeBack(move)
+		}
+		position.TakeBack(move)
 
-			if p.game.clock.stopSearch {
-				p.game.nodes += moveCount
-				//Log("searchRoot: bestMove %s pv[0][0] %s alpha %d\n", bestMove, p.game.pv[0][0], alpha)
-				return alpha
-			}
+		if p.game.clock.stopSearch {
+			p.game.nodes += moveCount
+			//Log("searchRoot: bestMove %s pv[0][0] %s alpha %d\n", bestMove, p.game.pv[0][0], alpha)
+			return alpha
+		}
 
-			if moveCount == 1 {
-				bestMove = move
-				p.game.pv[ply] = p.game.pv[ply][:0]
-				p.game.saveBest(ply, move)
-			}
+		if moveCount == 1 {
+			bestMove = move
+			p.game.pv[ply] = p.game.pv[ply][:0]
+			p.game.saveBest(ply, move)
+		}
 
-			if score > alpha {
-				alpha = score
-				bestMove = move
-				cacheFlags = cacheExact
-				p.game.saveBest(ply, move)
+		if score > alpha {
+			alpha = score
+			bestMove = move
+			cacheFlags = cacheExact
+			p.game.saveBest(ply, move)
 
-				if alpha >= beta {
-					cacheFlags = cacheBeta
-					break
-				}
+			if alpha >= beta {
+				cacheFlags = cacheBeta
+				break
 			}
 		}
 	}
