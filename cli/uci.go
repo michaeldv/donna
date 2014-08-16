db1a9622fb194eb12be492dbc6944d10f0ab69fd
Michael Dvorkin <mike@dvorkin.net>
2014-08-16 15:35:15 -0700

    Convert moves from coordinate notation in UCI position command

--- cli/uci.go
+++ cli/uci.go
@@ -19,24 +19,24 @@ func Uci() {
 	var position *donna.Position
 
 	// "uci" command handler.
-	doUci := func(g *donna.Game, args []string) {
+	doUci := func(args []string) {
 		fmt.Println(`Donna v1.0.0 Copyright (c) 2014 by Michael Dvorkin. All Rights Reserved.`)
 		fmt.Println(`id author Michael Dvorkin`)
 		fmt.Println(`uciok`)
 	}
 
 	// "ucinewgame" command handler.
-	doUciNewGame := func(g *donna.Game, args []string) {
+	doUciNewGame := func(args []string) {
 		game, position = nil, nil
 	}
 
 	// "isready" command handler.
-	doIsReady := func(g *donna.Game, args []string) {
+	doIsReady := func(args []string) {
 		fmt.Println(`readyok`)
 	}
 
 	// "position [startpos | fen ] [ moves ... ]" command handler.
-	doPosition := func(g *donna.Game, args []string) {
+	doPosition := func(args []string) {
 		fmt.Printf("%q\n", args)
 
 		// Make sure we've started the game since "ucinewgame" is optional.
@@ -50,16 +50,16 @@ func Uci() {
 			args = args[1:]
 			position = donna.NewInitialPosition(game)
 		case `fen`:
-			fen := ``
+			fen := []string{}
 			for _, token := range args[1:] {
 				args = args[1:] // Shift the token.
 				if token == `moves` {
 					break
 				}
-				fen += token + ` `
+				fen = append(fen, token)
 			}
-			fmt.Printf("fen: %s\n", fen)
-			position = donna.NewPositionFromFEN(game, fen)
+			fmt.Printf("fen: %s\n", strings.Join(fen, ` `))
+			position = donna.NewPositionFromFEN(game, strings.Join(fen, ` `))
 		default: return
 		}
 
@@ -67,13 +67,13 @@ func Uci() {
 		if position != nil && len(args) > 0 && args[0] == `moves` {
 			for _, move := range args[1:] {
 				args = args[1:] // Shift the move.
-				position = position.MakeMove(donna.NewMoveFromString(position, move))
+				position = position.MakeMove(donna.NewMoveFromNotation(position, move))
 			}
 		}
 		fmt.Printf("%s\n", position)
 	}
 
-	var commands = map[string]func(*donna.Game, []string){
+	var commands = map[string]func([]string){
 		`isready`: doIsReady,      
 		`uci`: doUci,
 		`ucinewgame`: doUciNewGame,
@@ -88,7 +88,7 @@ func Uci() {
 			break
 		}
 		if handler, found := commands[args[0]]; found {
-			handler(game, args[1:])
+			handler(args[1:])
 		}
 	}
 }
