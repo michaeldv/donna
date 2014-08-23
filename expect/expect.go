95ea2eb13013e1b981383b9cf53d20424d96cf9d
Michael Dvorkin <mike@dvorkin.net>
2014-08-22 23:46:03 -0700

    More robust expect

--- expect/expect.go
+++ expect/expect.go
@@ -4,7 +4,7 @@
 
 package expect
 
-import(`fmt`; `runtime`; `strings`; `testing`)
+import(`fmt`; `runtime`; `path/filepath`; `strings`; `testing`)
 
 func Eq(t *testing.T, actual, expected interface{}) {
 	log(t, actual, expected, equal(actual, expected))
@@ -19,11 +19,11 @@ func False(t *testing.T, actual interface{}) {
 }
 
 func Contain(t *testing.T, actual interface{}, expected string) {
-	match(t, fmt.Sprintf(`%v`, actual), expected, true)
+	match(t, fmt.Sprintf(`%+v`, actual), expected, true)
 }
 
 func NotContain(t *testing.T, actual interface{}, expected string) {
-	match(t, fmt.Sprintf(`%v`, actual), expected, false)
+	match(t, fmt.Sprintf(`%+v`, actual), expected, false)
 }
 
 func equal(actual, expected interface{}) (passed bool) {
@@ -48,8 +48,8 @@ func equal(actual, expected interface{}) (passed bool) {
 
 // Simple success/failure logger that assumes source test file is at Caller(2).
 func log(t *testing.T, actual, expected interface{}, passed bool) {
-	_, file, line, _ := runtime.Caller(2)          // Get the calling file path and line number.
-	file = file[strings.LastIndex(file, `/`) + 1:] // Keep file name only.
+	_, file, line, _ := runtime.Caller(2) 	// Get the calling file path and line number.
+	file = filepath.Base(file) 		// Keep file name only.
 
 	if !passed {
 		t.Errorf("\r\t\x1B[31m%s line %d\nExpected: %v\n  Actual: %v\x1B[0m", file, line, expected, actual)
@@ -62,7 +62,7 @@ func match(t *testing.T, actual, expected string, contains bool) {
 	passed := (contains == strings.Contains(actual, expected))
 
 	_, file, line, _ := runtime.Caller(2)
-	file = file[strings.LastIndex(file, `/`) + 1:]
+	file = filepath.Base(file)
 
 	if !passed {
 		t.Errorf("\r\t\x1B[31m%s line %d\nContains: %s\n  Actual: %s\x1B[0m", file, line, expected, actual)
