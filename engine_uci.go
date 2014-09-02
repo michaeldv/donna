ac20b0eb956f731efd65bf1860c75a045863b24b
Michael Dvorkin <mike@dvorkin.net>
2014-09-01 22:23:17 -0700

    More UCI tweaks

--- engine_uci.go
+++ engine_uci.go
@@ -7,7 +7,6 @@ package donna
 import(
 	`bufio`
 	`io`
-	`fmt`
 	`os`
 	`strconv`
 	`strings`
@@ -19,11 +18,14 @@ func (e *Engine) Uci() *Engine {
 	var game *Game
 	var position *Position
 
+	e.uci = true
+
 	// "uci" command handler.
 	doUci := func(args []string) {
-		fmt.Println(`Donna v1.0.0 Copyright (c) 2014 by Michael Dvorkin. All Rights Reserved.`)
-		fmt.Println(`id author Michael Dvorkin`)
-		fmt.Println(`uciok`)
+		e.reply("Donna v1.0.0 Copyright (c) 2014 by Michael Dvorkin. All Rights Reserved.\n")
+		e.reply("id name Donna v1.0.0\n")
+		e.reply("id author Michael Dvorkin\n")
+		e.reply("uciok\n")
 	}
 
 	// "ucinewgame" command handler.
@@ -33,13 +35,11 @@ func (e *Engine) Uci() *Engine {
 
 	// "isready" command handler.
 	doIsReady := func(args []string) {
-		fmt.Println(`readyok`)
+		e.reply("readyok\n")
 	}
 
 	// "position [startpos | fen ] [ moves ... ]" command handler.
 	doPosition := func(args []string) {
-		fmt.Printf("%q\n", args)
-
 		// Make sure we've started the game since "ucinewgame" is optional.
 		if game == nil || position == nil {
 			game = NewGame()
@@ -49,7 +49,7 @@ func (e *Engine) Uci() *Engine {
 		switch args[0] {
 		case `startpos`:
 			args = args[1:]
-			position = NewInitialPosition(game)
+			position = game.Start()
 		case `fen`:
 			fen := []string{}
 			for _, token := range args[1:] {
@@ -59,29 +59,23 @@ func (e *Engine) Uci() *Engine {
 				}
 				fen = append(fen, token)
 			}
-			fmt.Printf("fen: %s\n", strings.Join(fen, ` `))
 			position = NewPositionFromFEN(game, strings.Join(fen, ` `))
 		default: return
 		}
 
-		fmt.Printf("args: %q\n%s\n", args, position)
 		if position != nil && len(args) > 0 && args[0] == `moves` {
 			for _, move := range args[1:] {
 				args = args[1:] // Shift the move.
 				position = position.MakeMove(NewMoveFromNotation(position, move))
 			}
 		}
-		fmt.Printf("%s\n", position)
 	}
 
 	// "go [[wtime winc | btime binc ] movestogo] | depth | nodes | movetime"
 	doGo := func(args []string) {
 		options := e.options
-		fmt.Printf("%q\n", args)
-		fmt.Printf("-> e.options: %+v\n", e.options)
 
 		for i, token := range args {
-			fmt.Printf("\t%d len %d Token [%v]\n", i, len(args), token)
 			// Boolen "infinite" and "ponder" commands have no arguments.
 			if token == `infinite` {
 				options = Options{ infinite: true }
@@ -99,7 +93,7 @@ func (e *Engine) Uci() *Engine {
 					}
 				case `movetime`:
 					if n, err := strconv.Atoi(args[i+1]); err == nil {
-						options = Options{ maxDepth: n }
+						options = Options{ moveTime: int64(n) }
 					}
 				case `wtime`:
 					if position.color == White {
@@ -133,11 +127,11 @@ func (e *Engine) Uci() *Engine {
 			}
 		}
 		if options.timeLeft != 0 || options.timeInc != 0 || options.movesToGo != 0 {
-			e.variableLimits(options)
+			e.varyingLimits(options)
 		} else {
 			e.fixedLimit(options)
 		}
-		fmt.Printf("=> e.options: %+v\n", e.options)
+		game.Think()
 	}
 
 	// Stop calculating as soon as possible.
@@ -158,9 +152,12 @@ func (e *Engine) Uci() *Engine {
 	for {
 		command, err := bio.ReadString('\n')
 		if err != io.EOF && len(command) > 0 {
+			e.debug("> " + command)
 			args := strings.Split(command[:len(command)-1], ` `)
 			if args[0] == `quit` {
 				break
+			} else if args[0] == `debug` {
+				engine.log = true
 			}
 			if handler, ok := commands[args[0]]; ok {
 				handler(args[1:])
