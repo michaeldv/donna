5d60ff4dafd3df7e776f84c8bc02d090d132edfa
Michael Dvorkin <mike@dvorkin.net>
2015-10-11 20:48:23 -0700

    Version 3.0 release

--- data.go
+++ data.go
@@ -18,7 +18,7 @@ import `regexp`
 //                                      -― FORTRAN manual for Xerox Computers
 
 // Your chess engine hates you when you are working on a new version.
-const Version = `3.0rc1`
+const Version = `3.0`
 
 // Limits and conventions.
 const (
