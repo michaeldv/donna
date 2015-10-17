c72d17f8b4a24c8df322480d38ed6c52e1531a93
Michael Dvorkin <mike@dvorkin.net>
2015-10-17 11:33:52 -0700

    Adjusted evaluation tests

--- position_test.go
+++ position_test.go
@@ -166,20 +166,20 @@ func TestPosition250(t *testing.T) {
 // Restricted mobility for pinned pieces.
 func TestPosition300(t *testing.T) {
 	p := NewGame(`Ka1,a2,Nc3`, `Kh8,h7,Bg8`).start() // Nc3 vs Bishop, no pin.
-	expect.Eq(t, p.Evaluate(), -18)
+	expect.Eq(t, p.Evaluate(), -20)
 	p = NewGame(`Ka1,a2,Nc3`, `Kh8,h7,Bg7`).start() // Nc3 vs Bishop, pin on C3-G7 diagonal.
-	expect.Eq(t, p.Evaluate(), -67)
+	expect.Eq(t, p.Evaluate(), -69)
 
 }
 
 func TestPosition310(t *testing.T) {
 	p := NewGame(`Ka1,a2,Bc3`, `Kg8,h7,Bg6`).start() // Bc3 vs Bishop, no pin.
-	expect.Eq(t, p.Evaluate(), 0)
+	expect.Eq(t, p.Evaluate(), -2)
 	p = NewGame(`Ka1,a2,Bc3`, `Kg8,h7,Bg7`).start() // Bc3 vs Bishop, pin on C3-G7 diagonal.
-	expect.Eq(t, p.Evaluate(), -24)
+	expect.Eq(t, p.Evaluate(), -25)
 	p = NewGame(`Ka3,a2,Bc3`, `Kh8,h7,Rh1`).start() // Bc3 vs Rook, no pin.
-	expect.Eq(t, p.Evaluate(), -211)
+	expect.Eq(t, p.Evaluate(), -213)
 	p = NewGame(`Ka3,a2,Bc3`, `Kh8,h7,Rh3`).start() // Bc3 vs Rook, pin on C3-H3 file.
-	expect.Eq(t, p.Evaluate(), -317)
+	expect.Eq(t, p.Evaluate(), -319)
 
 }
