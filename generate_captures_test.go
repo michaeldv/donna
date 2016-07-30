36d07a759605746271125a9297fce0440aadd755
Michael Dvorkin <mike@dvorkin.net>
2016-07-30 15:45:33 -0700

    Move legality tests when generating captures

--- generate_captures_test.go
+++ generate_captures_test.go
@@ -52,19 +52,30 @@ func TestGenCaptures030(t *testing.T) {
 
 // Captures/promotions sort order.
 func TestGenCaptures100(t *testing.T) {
-	game := NewGame(`Kg1,Qh4,Rg2,Rf1,Nd6,f7`, `Kh8,Qc3,Rd8,Rd7,Ne6,a3,h7`)
-	gen := NewMoveGen(game.start()).generateCaptures()
+	p := NewGame(`Kg1,Qh4,Rg2,Rf1,Nd6,f7`, `Kh8,Qc3,Rd8,Rd7,Ne6,a3,h7`).start()
+	gen := NewMoveGen(p).generateCaptures()
 	expect.Eq(t, gen.allMoves(), `[f7-f8Q Qh4xh7 Qh4xd8]`)
 }
 
 func TestGenCaptures110(t *testing.T) {
-	game := NewGame(`Kg1,Qh4,Rg2,Rf1,Nd6,f7`, `Kh8,Qc3,Rd8,Rd7,Ne6,a3,h7`)
-	gen := NewMoveGen(game.start()).generateCaptures().rank(Move(0))
+	p := NewGame(`Kg1,Qh4,Rg2,Rf1,Nd6,f7`, `Kh8,Qc3,Rd8,Rd7,Ne6,a3,h7`).start()
+	gen := NewMoveGen(p).generateCaptures().rank(Move(0))
 	expect.Eq(t, gen.allMoves(), `[f7-f8Q Qh4xd8 Qh4xh7]`)
 }
 
 func TestGenCaptures120(t *testing.T) {
-	game := NewGame(`Kg1,Qh4,Rg2,Rf1,Nd6,f7`, `Kh8,Qc3,Rd8,Rd7,Ne6,a3,h7`)
-	gen := NewMoveGen(game.start()).generateCaptures().quickRank()
+	p := NewGame(`Kg1,Qh4,Rg2,Rf1,Nd6,f7`, `Kh8,Qc3,Rd8,Rd7,Ne6,a3,h7`).start()
+	gen := NewMoveGen(p).generateCaptures().quickRank()
 	expect.Eq(t, gen.allMoves(), `[f7-f8Q Qh4xd8 Qh4xh7]`)
+}
+
+// Move legality.
+func TestGenCaptures130(t *testing.T) {
+	p := NewGame(`Ka1,Rd8,Nb1,f6,h6`, `Kh8,Re1,Ng8,a3,c3`).start()
+	white := NewMoveGen(p).generateCaptures().quickRank()
+	expect.Eq(t, white.allMoves(), `[Rd8xg8 Nb1xc3 Nb1xa3]`) // Nb1 is pinned but nevertheless.
+
+	p.color = Black
+	black := NewMoveGen(p).generateCaptures()
+	expect.Eq(t, black.allMoves(), `[Re1xb1 Ng8xf6 Ng8xh6]`) // Ng8 is pinned but nevertheless.
 }
